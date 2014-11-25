d8f55a610dbe184822ac7ee039c6b58c4ad47da1
Phil Jirsa <phil@36mph.com>
2014-11-25 12:38:13 -0600

    added proxy calls to ICNDB using nancyfx

---
+++ ChuckNorris.Api/ChuckNorrisModule.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ChuckNorris.Api
+{
+    public class ChuckNorrisModule : NancyModule
+    {
+        private const string BASE_API = "http://api.icndb.com/";
+
+        public ChuckNorrisModule()
+        {
+            Get["/"] = _ =>
+                {
+                    return "Hello from Chuck Norris API.";
+                };
+
+            Get["/jokes/{id:int}"] = _ =>
+                {
+                    return GetResponse(BASE_API + "jokes/" + _.id);
+                };
+
+            Get["/jokes"] = _ => 
+            {
+                string reqUrl = GetRequestUrl("jokes", Request.Query);
+                return GetResponse(reqUrl);
+            };
+
+            Get["/jokes/random"] = _ =>
+                {
+                    string reqUrl = GetRequestUrl("jokes/random", Request.Query);
+                    return GetResponse(reqUrl);
+                };
+        }
+
+        #region Helpers
+
+        private string GetRequestUrl(string endpoint, DynamicDictionary query)
+        {
+            string reqUrl = BASE_API + endpoint;
+            if (Request.Query.Count > 0)
+            {
+                reqUrl += "?";
+                foreach (var qs in Request.Query)
+                {
+                    reqUrl += qs + "=" + Request.Query[qs] + "&";
+                }
+            }
+            return reqUrl;
+        }
+
+        private Response GetResponse(string requestUrl)
+        {
+            var req = WebRequest.CreateHttp(requestUrl);
+            HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
+            StreamReader reader = new StreamReader(resp.GetResponseStream());
+            string json = reader.ReadToEnd();
+            var result = (Response)json;
+            result.ContentType = "application/json";
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ ChuckNorris.Api/Models.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChuckNorris.Api
+{
+    public class Joke
+    {
+        //public int Id { get; set; }
+        //public string Joke { get; set; }
+        //public IList<string> Categories { get; set; }
+    }
+}
